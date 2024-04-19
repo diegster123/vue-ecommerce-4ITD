efb56849bdb3f9629c0361f45ccf5e1b4a137a2c
diegster123 <diegojumagdao@gmail.com>
2024-04-19 19:31:49 +0800

    added server.js and eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		"plugin:vue/vue3-essential",
+		"@vue/standard",
+		"@vue/typescript/recommended",
+	],
+	parserOptions: {
+		ecmaVersion: 2020,
+	},
+	rules: {
+		// Add custom ESLint rules here
+	},
+};
