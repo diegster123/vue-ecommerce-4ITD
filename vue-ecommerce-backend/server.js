7c5b215785c5591214d53a49af9d6d43994b4c3c
diegster123 <diegojumagdao@gmail.com>
2024-04-21 02:25:07 +0800

    changed server to port 4000. added authRoutes in routes

--- vue-ecommerce-backend/server.js
+++ vue-ecommerce-backend/server.js
@@ -15,9 +15,10 @@ app.use(cors());
 
 // Routes
 app.use("/api/products", productRoutes);
+app.use("/api/auth", authRoutes);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
