c1cbcb7d991cc1e830bf47913714e90dc8a1a506
diegster123 <diegojumagdao@gmail.com>
2024-04-23 23:51:13 +0800

    added routing in index.js

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+// router/index.js
+
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginForm from '../components/LoginForm.vue' // Import your LoginForm component
+// import Home from "../components/Home.vue"; // Example component for home page
+
+const routes = [
+  // {
+  // 	path: "/",
+  // 	name: "Home",
+  // 	component: Home,
+  // },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginForm
+  }
+  // Add more routes as needed
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
