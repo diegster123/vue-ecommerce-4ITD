9202ac10a64d390d0929bb395d93bc4a6021a3f8
diegster123 <diegojumagdao@gmail.com>
2024-04-24 00:18:01 +0800

    disabled some eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,13 @@ module.exports = {
 	},
 	rules: {
 		"no-tabs": 0,
+		indent: "off", // Disable indentation checking
+		quotes: "off", // Disable singlequote checking
+		"comma-dangle": "off", // Disable trailing comma checking
+		semi: "off", // Disable semicolon checking
+		"no-trailing-spaces": "off", // Disable trailing spaces checking
+		"no-mixed-spaces-and-tabs": "off", // Disable mixed spaces and tabs checking
+		"no-multiple-empty-lines": "off", // Disable multiple empty lines checking
+		"eol-last": "off", // Disable checking for newline at the end of file
 	},
 };
