0ab9357c56612a42464f85eaf4f20845dfb3e51c
diegster123 <diegojumagdao@gmail.com>
2024-04-24 00:15:43 +0800

    added HomePage for PETPALS

---
+++ src/components/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <h1>PETPALS</h1>
+      <button @click="redirectToLogin">Login</button>
+    </div>
+  </template>
+
+<script>
+export default {
+  methods: {
+    redirectToLogin () {
+      this.$router.push({ name: 'Login' })
+    }
+  }
+}
+</script>
+
+  <style>
+  /* Add your styles here */
+  </style>
